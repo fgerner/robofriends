898133797f249ee4f41203941fd2320c04dbfcc4
fredrikgerner <fred.gerner@icloud.com>
2021-03-28 12:00:07 +1100

    added cardlist component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card'
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import {robots} from "./robots";
 
 
 ReactDOM.render(
-    <div>
-        <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
-    </div>
+    <CardList robots={robots}/>
     , document.getElementById('root')
 )
 ;
