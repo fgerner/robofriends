03a9405f8b276c0741c80ac6ee61ddd3b4b123fb
fredrikgerner <fred.gerner@icloud.com>
2021-04-10 22:45:52 +1000

    added action and reducer

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,7 +31,7 @@ class App extends Component {
         return !robots.length ?
             <h1 className={'tc f1'}>Loading...</h1>
             :
-            (
+            ( 
                 <div className={'tc'}>
                     <h1 className={'f1'}>RoboFriends</h1>
                     <SearchBox searchChange={this.onSearchChange}/>
